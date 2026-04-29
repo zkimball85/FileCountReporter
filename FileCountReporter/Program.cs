d659df73f4919f8707aaf1bf7969a948de980c7c
Zac Kimball <z.kimball85@gmail.com>
2026-04-29 13:10:03 -0700

    Add project files.

---
+++ FileCountReporter/Program.cs
@@ -0,0 +1,11 @@
+﻿///<summary> 
+///The goal of this application will be to create a console application
+///or a Windows Forms app that can read a target directory (folder) and 
+///report how many files of each type it can find. Example: Search the 
+///downloads folder and report how many .txt files there are. Since we 
+///have not covered some of the skills you will need, you are encouraged 
+///to use GitHub Copilot or another AI to help you create a solution. 
+///Add the project to source control when you start.
+///</summary>
+///
+
