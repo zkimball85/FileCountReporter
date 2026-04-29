94f9ad2200dbe08521b97f75234a9da2d7a7efdb
Zac Kimball <z.kimball85@gmail.com>
2026-04-29 15:26:26 -0700

    Refactor UI layout and update control properties
    Refactored Form1.Designer.cs to use implicit variable references and simplified event handler assignments. Updated UI layout with larger controls, adjusted positions and margins, increased form size, set AcceptButton to btnCountFiles, and made RichTextBox read-only. Form1.resx changes are limited to whitespace and formatting in XML comments.

--- FileCountReporter/Form1.Designer.cs
+++ FileCountReporter/Form1.Designer.cs
@@ -28,61 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnSelectFolder = new System.Windows.Forms.Button();
-            this.txtSelectedFolder = new System.Windows.Forms.TextBox();
-            this.btnCountFiles = new System.Windows.Forms.Button();
-            this.rtbOutput = new System.Windows.Forms.RichTextBox();
-            this.SuspendLayout();
+            btnSelectFolder = new Button();
+            txtSelectedFolder = new TextBox();
+            btnCountFiles = new Button();
+            rtbOutput = new RichTextBox();
+            SuspendLayout();
             // 
             // btnSelectFolder
             // 
-            this.btnSelectFolder.Location = new System.Drawing.Point(12, 12);
-            this.btnSelectFolder.Name = "btnSelectFolder";
-            this.btnSelectFolder.Size = new System.Drawing.Size(120, 23);
-            this.btnSelectFolder.TabIndex = 0;
-            this.btnSelectFolder.Text = "Select Folder";
-            this.btnSelectFolder.UseVisualStyleBackColor = true;
-            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            btnSelectFolder.Location = new Point(22, 26);
+            btnSelectFolder.Margin = new Padding(6, 6, 6, 6);
+            btnSelectFolder.Name = "btnSelectFolder";
+            btnSelectFolder.Size = new Size(223, 49);
+            btnSelectFolder.TabIndex = 0;
+            btnSelectFolder.Text = "Select Folder";
+            btnSelectFolder.UseVisualStyleBackColor = true;
+            btnSelectFolder.Click += btnSelectFolder_Click;
             // 
             // txtSelectedFolder
             // 
-            this.txtSelectedFolder.Location = new System.Drawing.Point(138, 14);
-            this.txtSelectedFolder.Name = "txtSelectedFolder";
-            this.txtSelectedFolder.ReadOnly = true;
-            this.txtSelectedFolder.Size = new System.Drawing.Size(534, 23);
-            this.txtSelectedFolder.TabIndex = 1;
+            txtSelectedFolder.Location = new Point(256, 30);
+            txtSelectedFolder.Margin = new Padding(6, 6, 6, 6);
+            txtSelectedFolder.Name = "txtSelectedFolder";
+            txtSelectedFolder.ReadOnly = true;
+            txtSelectedFolder.Size = new Size(988, 39);
+            txtSelectedFolder.TabIndex = 1;
             // 
             // btnCountFiles
             // 
-            this.btnCountFiles.Location = new System.Drawing.Point(678, 12);
-            this.btnCountFiles.Name = "btnCountFiles";
-            this.btnCountFiles.Size = new System.Drawing.Size(110, 23);
-            this.btnCountFiles.TabIndex = 2;
-            this.btnCountFiles.Text = "Count Files";
-            this.btnCountFiles.UseVisualStyleBackColor = true;
-            this.btnCountFiles.Click += new System.EventHandler(this.btnCountFiles_Click);
+            btnCountFiles.Location = new Point(1259, 26);
+            btnCountFiles.Margin = new Padding(6, 6, 6, 6);
+            btnCountFiles.Name = "btnCountFiles";
+            btnCountFiles.Size = new Size(204, 49);
+            btnCountFiles.TabIndex = 2;
+            btnCountFiles.Text = "Count Files";
+            btnCountFiles.UseVisualStyleBackColor = true;
+            btnCountFiles.Click += btnCountFiles_Click;
             // 
             // rtbOutput
             // 
-            this.rtbOutput.Location = new System.Drawing.Point(12, 41);
-            this.rtbOutput.Name = "rtbOutput";
-            this.rtbOutput.Size = new System.Drawing.Size(776, 397);
-            this.rtbOutput.TabIndex = 3;
-            this.rtbOutput.Text = "";
+            rtbOutput.Location = new Point(22, 87);
+            rtbOutput.Margin = new Padding(6, 6, 6, 6);
+            rtbOutput.Name = "rtbOutput";
+            rtbOutput.ReadOnly = true;
+            rtbOutput.Size = new Size(1438, 842);
+            rtbOutput.TabIndex = 3;
+            rtbOutput.Text = "";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.rtbOutput);
-            this.Controls.Add(this.btnCountFiles);
-            this.Controls.Add(this.txtSelectedFolder);
-            this.Controls.Add(this.btnSelectFolder);
-            this.Name = "Form1";
-            this.Text = "File Count Reporter";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            AcceptButton = btnCountFiles;
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1486, 960);
+            Controls.Add(rtbOutput);
+            Controls.Add(btnCountFiles);
+            Controls.Add(txtSelectedFolder);
+            Controls.Add(btnSelectFolder);
+            Margin = new Padding(6, 6, 6, 6);
+            Name = "Form1";
+            Text = "File Count Reporter";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
