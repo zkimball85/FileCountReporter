eaafa11dade688649c3a02e207ab271cafd8bb2a
Zac Kimball <z.kimball85@gmail.com>
2026-04-29 13:14:44 -0700

    Convert project to Windows Forms app with Form1 scaffold
    Converted from console to Windows Forms by updating project settings and entry point. Added Form1 with designer and resource files as the main form.

--- FileCountReporter/Program.cs
+++ FileCountReporter/Program.cs
@@ -1,11 +1,17 @@
-﻿///<summary> 
-///The goal of this application will be to create a console application
-///or a Windows Forms app that can read a target directory (folder) and 
-///report how many files of each type it can find. Example: Search the 
-///downloads folder and report how many .txt files there are. Since we 
-///have not covered some of the skills you will need, you are encouraged 
-///to use GitHub Copilot or another AI to help you create a solution. 
-///Add the project to source control when you start.
-///</summary>
-///
-
+namespace FileCountReporter
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
